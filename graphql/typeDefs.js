93d64d47faae3b108b4e2644cd82f91731a93db2
AmyBraswell <braswellamy@mac.com>
2020-06-04 15:35:38 -0500

    looking for differences

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -45,7 +45,7 @@ module.exports = gql`
   }
   type Mutation {
     register(registerInput: RegisterInput): User!
-    login(username: String!, password: String!): User!
+    login(email: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
     createComment(postId: ID!, body: String!): Post!
